972550abd7a3d8e1595ca27b36d867cf0a002d60
zelermehdi <gamouh.mehdi@gmail.com>
2022-06-23 11:43:20 +0200

    questions 5.6

--- index.php
+++ index.php
@@ -67,9 +67,9 @@ private $categories;
 
 
 $produit1= new Product( "PC Portable Gaming MSI GL75 Leopard 10SFK-457FR 17,3 Intel Core i7 16 Go RAM 256 Go SSD + 1 To SATA Noir",1999 , "oui", 33,"ordinateurs portables");
- $produit2=new Product("PC Portable Gaming Asus TUF505DV-HN232T 15.6 AMD Ryzen 7 16 Go RAM 512 Go SSD Noir",1499 ,"Oui", 33,"Ordinateurs Portables");
+ $produit2=new Product("PC Portable Gaming Asus TUF505DV-HN232T 15.6 AMD Ryzen 7 16 Go RAM 512 Go SSD Noir",1499 ,"Oui", 0.33,"Ordinateurs Portables");
  $produit3=new product("MICHAËL GREGORIO",43,"non","non","Spectacles Rodez");
- $produit4=new product("DANIEL GUICHARD",43,"non",20,"Spectacles Rodez");
+ $produit4=new product("DANIEL GUICHARD",43,"non",0.2,"Spectacles Rodez");
  $produit5=new product("PC Portable Gaming Acer Predator Triton 700 PT715-51-76D4 15.6 Gaming Intel Core i7 32 Go RAM 256 Go SSD + 256 Go SATA Noir",3499,"non","non","Ordinateurs Portables");
 
 $tableProduct=[$produit1,$produit2,$produit3,$produit4,$produit5];
@@ -85,14 +85,30 @@ foreach($tableProduct as $product) {
 
 // 5/ Afficher la liste des produits bénéficiant d’une remise. Informations affichées : nom du produit, montant sans remise, montant avec remise
 
+
+// fonction pour calculer la remise 
+
+
+function discountprice($price, $discount){
+$discountPrice = $price-($price*$discount);
+return $discountPrice;
+}
+
+
 foreach($tableProduct as $product ) {
-  if($product->getDiscount()>1){
-    echo $product->getName()."</br>";
+  if($product->getDiscount()!=="non"){
+    echo   $product->getName(),$product->getprice(),discountprice($product->getprice(),$product->getDiscount())."</br>"  ;
+
 }
 }
 
-// var_dump($produit1,$produit2,$produit3,$produit4,$produit5);
- 
+
+
+// 6/ Afficher la liste des produits triés par leur nom. Information affichée : nom du produit
+
+$alphabet= ["PC Portable Gaming MSI GL75 Leopard 10SFK-457FR 17,3 Intel Core i7 16 Go RAM 256 Go SSD + 1 To SATA Noir","PC Portable Gaming Asus TUF505DV-HN232T 15.6 AMD Ryzen 7 16 Go RAM 512 Go SSD Noir","MICHAËL GREGORIO" ,"DANIEL GUICHARD","PC Portable Gaming Acer Predator Triton 700 PT715-51-76D4 15.6 Gaming Intel Core i7 32 Go RAM 256 Go SSD + 256 Go SATA Noir"];
+sort($alphabet);
+print_r($alphabet);
 
 
 
@@ -100,26 +116,17 @@ foreach($tableProduct as $product ) {
 
 
 
-
-// echo $produit1->getCategories();
-
-
-
-
-
-// $produitPromotion->setName("mon nouveau nom");
-
-
-
-
-
-
-//  echo $tablePromotion->getName();
-
-
-
-// var_dump($tablePromotion);
-// // $produitRemise= new Product("DANIEL GUICHARD",43, );
 
 
 ?>
+
+
+
+
+
+
+
+
+
+
+
